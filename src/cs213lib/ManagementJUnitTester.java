e82626b8e8077cc3594e96f275417beeba64cf27
Aryak <pandeary12@gmail.com>
2021-02-22 21:02:37 -0500

    added ManagementJUnitTester 2

--- src/cs213lib/ManagementJUnitTester.java
+++ src/cs213lib/ManagementJUnitTester.java
@@ -9,7 +9,7 @@ public class ManagementJUnitTester {
     @DisplayName("Test Management calculatePayment() method")
     public void assertCalculatePayment(){
 
-
+        Management m = new Management(new Profile("Jane,Doe","IT",new Date("5/17/2020")),90000,1);
 
     }
 
